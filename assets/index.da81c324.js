715b0632c1efeaa8fadec62766c9c947b046cbaf
zjw977020546 <11487928+zjw977020546@user.noreply.gitee.com>
2022-10-18 23:20:28 +0800

    init

--- assets/index.da81c324.js
+++
@@ -1,1 +0,0 @@
-import{d as t,o as a,c as s,a as n,t as o}from"./index.61dc0435.js";const i={__name:"index",setup(r){const e=t("\u641C\u7D22\u9875");return(c,l)=>(a(),s("div",null,[n("h1",null,o(e.value),1)]))}};export{i as default};
