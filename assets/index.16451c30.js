7fd73115559557459fcb26ca9d7bcca441051b9a
zjw977020546 <11487928+zjw977020546@user.noreply.gitee.com>
2022-10-18 22:31:31 +0800

    init

---
+++ assets/index.16451c30.js
@@ -0,0 +1,1 @@
+import{d as t,o as a,c as s,a as n,t as o}from"./index.6bd07492.js";const i={__name:"index",setup(r){const e=t("\u641C\u7D22\u9875");return(c,l)=>(a(),s("div",null,[n("h1",null,o(e.value),1)]))}};export{i as default};
---
+++ assets/index.2e1a0bbc.js
@@ -0,0 +1,1 @@
+import{d as t,o as a,c as s,a as n,t as o}from"./index.6bd07492.js";const i={__name:"index",setup(r){const e=t("\u6211\u7684");return(c,l)=>(a(),s("div",null,[n("h1",null,o(e.value),1)]))}};export{i as default};
